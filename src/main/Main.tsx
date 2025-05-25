39fb1f88183ec8c129b931b8da0e743abdffd2b2
Jay Shah <jaymintu2003@gmail.com>
2025-05-26 00:34:23 +0530

    Seperated Sidebar and Main content

---
+++ src/main/Main.tsx
@@ -0,0 +1,21 @@
+import './Main.css'
+
+function Main() {
+
+
+    return (
+        <>
+
+        <div className="MainContent">
+
+            <div className="list">
+                No tasks yet.....
+            </div>
+
+        </div>
+
+        </>
+    )
+}
+
+export default Main
---
+++ src/sidebar/Sidebar.tsx
@@ -0,0 +1,11 @@
+import './Sidebar.css'
+
+function Sidebar(){
+    return (
+        <>
+        <div>Here is the sidebar</div>
+        </>
+    )
+}
+
+export default Sidebar

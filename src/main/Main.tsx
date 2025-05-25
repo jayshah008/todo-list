ea32df59098122e4f7c9719ae64d19624ddb99fe
Jay Shah <jaymintu2003@gmail.com>
2025-05-26 01:15:53 +0530

    Added Listing feature using state of React

--- src/main/Main.tsx
+++ src/main/Main.tsx
@@ -1,7 +1,21 @@
 import './Main.css'
+import { useState } from 'react';
+
 
 function Main() {
 
+    const [ItemValue, setItemValue] = useState('');
+    const [Items, setItems] = useState<string[]>([]);
+
+    function addItems(){
+        setItems([...Items, ItemValue])
+        setItemValue('');
+    }
+
+    const listItems = Items.map(item => 
+        <li>{item}</li>
+    )
+        
 
     return (
         <>
@@ -11,6 +25,13 @@ function Main() {
             <div className="list">
                 No tasks yet.....
             </div>
+
+            <div className="listItems">
+                <ul>{listItems}</ul>
+            </div>
+
+            <input type="text" value = {ItemValue} onChange={(e) => {setItemValue(e.target.value)}}/>
+            <button onClick={addItems}>Add Task</button>
 
         </div>
 

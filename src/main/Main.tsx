682cb3a18b118820a39c6cb6636843a0248682b2
Jay Shah <jaymintu2003@gmail.com>
2025-05-29 23:57:47 +0530

    Added Date and created the sidebar

--- src/main/Main.tsx
+++ src/main/Main.tsx
@@ -1,10 +1,13 @@
 import './Main.css'
 import { useState } from 'react';
+import { CiCircleCheck } from "react-icons/ci";
+
 
 
 interface itemList{
     id : number;
     value : String;
+    dateValue : String;
 }
 
 function Main() {
@@ -12,11 +15,13 @@ function Main() {
     const [ItemValue, setItemValue] = useState('');
     const [Items, setItems] = useState<itemList[]>([]);
     const [id_Item, setId] = useState<number>(0);
+    const [date, setDate] = useState("");
 
     function addItems(){
-        setItems([...Items, {id : id_Item, value: ItemValue}])
+        setItems([...Items, {id : id_Item, value: ItemValue, dateValue : date}])
         setItemValue('');
         setId(id_Item => id_Item+1);
+        setDate("");
     }
 
     const removeItem = (ItemId : number) => {
@@ -24,7 +29,7 @@ function Main() {
     }
 
     const listItems = Items.map(item => 
-        <li>{item.value}<button onClick={() => removeItem(item.id)}>Finished</button></li>
+        <li> <CiCircleCheck  onClick={() => removeItem(item.id)}/> {item.value} - {item.dateValue}<button onClick={() => removeItem(item.id)}>Finished</button></li>
     )
         
 
@@ -33,16 +38,22 @@ function Main() {
 
         <div className="MainContent">
 
+            <div className="headerContent">
+                <h1 className='day'>Today</h1>
+                <h1 className='count'>{Items.length}</h1>
+            </div>
+
             <div className="listItems">
 
                 {Items.length == 0 ?
                 "No Items added yet..."
                 :
-                <ul>{listItems}</ul>
+                <ul className='listItems'>{listItems}</ul>
                 }
             </div>
 
             <input type="text" value = {ItemValue} onChange={(e) => {setItemValue(e.target.value)}}/>
+            <input type="date" value={date} onChange={(e) => {setDate(e.target.value)}}/>
             <button onClick={addItems}>Add Task</button>
 
         </div>
--- src/sidebar/Sidebar.tsx
+++ src/sidebar/Sidebar.tsx
@@ -3,7 +3,21 @@ import './Sidebar.css'
 function Sidebar(){
     return (
         <>
-        <div>Here is the sidebar</div>
+        <div className="mainContent">
+            <h1>Welcome!</h1>
+
+            <div className="sections">
+                <h3>Search</h3>
+                <h3>Today</h3>
+                <h3>Upcoming</h3>
+            </div>
+
+            <h1>Categories</h1>
+            <div className="categories">
+                <h3>Category</h3>
+            </div>
+
+        </div>
         </>
     )
 }
